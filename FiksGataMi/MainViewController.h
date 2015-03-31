8c1e84e31a7ba92a8044197d306a4a74b50153ed
Alexander Alemayhu <alexander@bitraf.no>
2015-03-31 18:23:26 +0200

    Forgot to include files in previous commit

---
+++ FiksGataMi/MainViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FiksGataMi
+//
+//  Created by Alexander Alemayhu on 31/03/15.
+//  Copyright (c) 2015 Alexander Alemayhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+
+@end
+
---
+++ FiksGataMi/MainViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  FiksGataMi
+//
+//  Created by Alexander Alemayhu on 31/03/15.
+//  Copyright (c) 2015 Alexander Alemayhu. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
