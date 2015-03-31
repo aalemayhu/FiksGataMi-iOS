906f074029393d3f72b5341d62f5444724660b54
Alexander Alemayhu <alexander@bitraf.no>
2015-03-31 18:24:51 +0200

    :fire: Remove noise from FiksGataMiTests

--- FiksGataMiTests/FiksGataMiTests.m
+++ FiksGataMiTests/FiksGataMiTests.m
@@ -1,11 +1,3 @@
-//
-//  FiksGataMiTests.m
-//  FiksGataMiTests
-//
-//  Created by Alexander Alemayhu on 31/03/15.
-//  Copyright (c) 2015 Alexander Alemayhu. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
 
@@ -17,23 +9,18 @@
 
 - (void)setUp {
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
 }
 
 - (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
 }
 
 - (void)testExample {
-    // This is an example of a functional test case.
     XCTAssert(YES, @"Pass");
 }
 
 - (void)testPerformanceExample {
-    // This is an example of a performance test case.
     [self measureBlock:^{
-        // Put the code you want to measure the time of here.
     }];
 }
 
