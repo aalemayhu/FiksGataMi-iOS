ce91d84fac197d6aeeb0d76c1e3735cf7871c43f
Alexander Alemayhu <alexander@bitraf.no>
2015-03-31 17:41:40 +0200

    Add xcode project

---
+++ FiksGataMi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FiksGataMi
+//
+//  Created by Alexander Alemayhu on 31/03/15.
+//  Copyright (c) 2015 Alexander Alemayhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
