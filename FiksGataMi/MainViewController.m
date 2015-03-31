9e98f117f3f4545e13f428628d02f1f0188f2e3b
Alexander Alemayhu <alexander@bitraf.no>
2015-03-31 18:36:32 +0200

    :fire: Remove noise from MainViewController

--- FiksGataMi/MainViewController.m
+++ FiksGataMi/MainViewController.m
@@ -1,11 +1,3 @@
-//
-//  ViewController.m
-//  FiksGataMi
-//
-//  Created by Alexander Alemayhu on 31/03/15.
-//  Copyright (c) 2015 Alexander Alemayhu. All rights reserved.
-//
-
 #import "MainViewController.h"
 
 @interface MainViewController ()
@@ -16,12 +8,10 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
